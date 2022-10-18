d7b81b90abd0500a3f38ed75806e239ddbb612ea
Paweł Mańczak <pawel14522@gmail.com>
2022-10-18 21:09:52 +0200

    buffer

--- simulation.adb
+++ simulation.adb
@@ -1,11 +1,5 @@
--- Author: Pawel Manczak, index 188756
--- 188756 % 3 = 2
--- A skeleton of a program for an assignment in programming languages
--- The students should rename the tasks of producers, consumers, and the buffer
--- Then, they should change them so that they would fit their assignments
--- They should also complete the code with constructions that lack there
 with Ada.Text_IO; use Ada.Text_IO;
-with Ada.Integer_Text_IO; 
+with Ada.Integer_Text_IO;
 with Ada.Numerics.Discrete_Random;
 
 
@@ -14,232 +8,220 @@ procedure Simulation is
    Number_Of_Assemblies: constant Integer := 3;
    Number_Of_Consumers: constant Integer := 2;
    
-   --custom types of variables
-   subtype Product_Type is Integer range 1 .. Number_Of_Products; -- ketchup 1,2,3,4,5 et
-   Product_Name: constant array (Product_Type) of String(1 .. 8)
-     := ("Product1", "Product2", "Product3", "Product4", "Product5");
-   
-   subtype Assembly_Type is Integer range 1 .. Number_Of_Assemblies; --rodzaj zestawu
-   Assembly_Name: constant array (Assembly_Type) of String(1 .. 9)
-     := ("Assembly1", "Assembly2", "Assembly3");
+   subtype Zakres_Czasu_Produkcji is Integer range 3 .. 6;
+   subtype Zakres_Czasu_Konsumpcji is Integer range 4 .. 8;
+   subtype Podprodukt_Type is Integer range 1 .. Number_Of_Products;
+   subtype Typ_Zestawow is Integer range 1 .. Number_Of_Assemblies;
+   subtype Typ_Konsumenta is Integer range 1 .. Number_Of_Consumers;
+   Nazwa_Wyrobu: constant array (Podprodukt_Type) of String(1 .. 6)
+     := ("Salata", "Ser   ", "Szynka", "Bulka ", "Chleb ");
+   Nazwa_Zestawu: constant array (Typ_Zestawow) of String(1 .. 14)
+     := ("Kanapka Drwala", "Kanapka Jarska", "Bulka Serowa  ");
+   package Losowa_Konsumpcja is new
+     Ada.Numerics.Discrete_Random(Zakres_Czasu_Konsumpcji);
+   package Losowy_Zestaw is new
+     Ada.Numerics.Discrete_Random(Typ_Zestawow);
 
-   subtype Consumer_Type is Integer range 1 .. Number_Of_Consumers;
-   
-   
-   package Random_Assembly is new
-     Ada.Numerics.Discrete_Random(Assembly_Type);
-   type My_Str is new String(1 ..256);
-   -- end of custom types
-   
-   -- Producer produces determined product, w nieskonczonosc produkuje, nigydn ie konczy
-   task type Producer is
-      -- Give the Producer an identity, i.e. the product type
-      entry Start(Product: in Product_Type; Production_Time: in Integer);
-   end Producer;
+   task type Producent is
+      entry Zacznij(Wyrob: in Podprodukt_Type; Czas_Produkcji: in Integer);
+   end Producent;
 
-   -- Consumer gets an arbitrary assembly of several products from the buffer
-   task type Consumer is
-      -- Give the Consumer an identity
-      entry Start(Consumer_Number: in Consumer_Type;
-		    Consumption_Time: in Integer);
-   end Consumer;
+   task type Konsument is
+      entry Zacznij(Numer_Konsumenta: in Typ_Konsumenta;
+                    Czas_Konsumpcji: in Integer);
+   end Konsument;
 
-   -- In the Buffer, products are assemblied into an assembly
-   task type Buffer is
-      -- Accept a product to the storage provided there is a room for it
-      entry Take(Product: in Product_Type; Number: in Integer);
-      -- Deliver an assembly provided there are enough products for it
-      entry Deliver(Assembly: in Assembly_Type; Number: out Integer);
-   end Buffer;
+   task type Bufor is
+      entry Przyjmij(Wyrob: in Podprodukt_Type; Numer: in Integer);
+      entry Wydaj(Zestaw: in Typ_Zestawow; Numer: out Integer; Wydano: out Boolean);
+   end Bufor;
 
-   P: array ( 1 .. Number_Of_Products ) of Producer;
-   K: array ( 1 .. Number_Of_Consumers ) of Consumer;
-   B: Buffer;
+   P: array ( 1 .. Number_Of_Products ) of Producent;
+   K: array ( 1 .. Number_Of_Consumers ) of Konsument;
+   B: Bufor;
 
-   task body Producer is
-      subtype Production_Time_Range is Integer range 3 .. 6;
-      package Random_Production is new
-	Ada.Numerics.Discrete_Random(Production_Time_Range);
-      G: Random_Production.Generator;	--  generator liczb losowych
-      Product_Type_Number: Integer;
-      Product_Number: Integer;
-      Production: Integer;
+   task body Producent is
+      package Losowa_Produkcja is new
+        Ada.Numerics.Discrete_Random(Zakres_Czasu_Produkcji);
+      G: Losowa_Produkcja.Generator;	--  generator liczb losowych
+      Nr_Typu_Wyrobu: Integer;
+      Numer_Wyrobu: Integer;
+      Produkcja: Integer;
+      PROD: Integer;
    begin
-      accept Start(Product: in Product_Type; Production_Time: in Integer) do
-         Random_Production.Reset(G);	--  start random number generator
-         Product_Number := 1;
-         Product_Type_Number := Product;
-         Production := Production_Time;
-      end Start;
-      Put_Line("Started producer of " & Product_Name(Product_Type_Number));
+      accept Zacznij(Wyrob: in Podprodukt_Type; Czas_Produkcji: in Integer) do
+         Losowa_Produkcja.Reset(G);	--  zacznij generator liczb losowych
+         Numer_Wyrobu := 1;
+         Nr_Typu_Wyrobu := Wyrob;
+         Produkcja := Czas_Produkcji;
+         PROD := Losowa_Produkcja.Random(G);
+      end Zacznij;
+      Put_Line("PRODUCENT: Rozpoczeto wyrob komponentu " & Nazwa_Wyrobu(Nr_Typu_Wyrobu));
       loop
-         Put_Line("Produced product " & Product_Name(Product_Type_Number)
-		   & " number "  & Integer'Image(Product_Number));
-	 -- Accept for storage
-	 B.Take(Product_Type_Number, Product_Number);
-	 Product_Number := Product_Number + 1;
+         Put_Line("PRODUCENT: Przewidywany czas produkcji to " & Integer'Image(PROD) & " s");
+         delay Duration(PROD);
+         Put_Line("PRODUCENT: Wyprodukowano komponent " & Nazwa_Wyrobu(Nr_Typu_Wyrobu)
+                  & " numer "  & Integer'Image(Numer_Wyrobu));
+         -- Wstaw do magazynu
+         loop
+            select
+               B.Przyjmij(Nr_Typu_Wyrobu, Numer_Wyrobu);
+               Numer_Wyrobu := Numer_Wyrobu + 1;
+               exit;
+            else
+               Put_Line("BUFOR: Bufor jest zajety, poczekaj 3s.");
+               delay Duration(3.0);
+            end select;
+         end loop;
       end loop;
-   end Producer;
+   end Producent;
 
-   task body Consumer is
-      subtype Consumption_Time_Range is Integer range 4 .. 8;
-      package Random_Consumption is new
-        Ada.Numerics.Discrete_Random(Consumption_Time_Range);
-      
-      G: Random_Consumption.Generator;	--  random number generator (time)
-      G2: Random_Assembly.Generator;	--  also (assemblies)
-      Consumer_Nb: Consumer_Type;
-      Assembly_Number: Integer;
-      Consumption: Integer;
-      Assembly_Type: Integer;
-      Consumer_Name: constant array (1 .. Number_Of_Consumers) of String(1 .. 9) := ("Consumer1", "Consumer2");
+   task body Konsument is
+      G: Losowa_Konsumpcja.Generator;	--  generator liczb losowych (czas)
+      G2: Losowy_Zestaw.Generator;
+      Nr_Konsumenta: Typ_Konsumenta;
+      Numer_Zestawu: Integer;
+      Konsumpcja: Integer;
+      Rodzaj_Zestawu: Integer;
+      CZAS: Integer;
+      Wydano: Boolean;
+      Nazwa_Konsumenta: constant array (1 .. Number_Of_Consumers)
+        of String(1 .. 9)
+        := ("Pani Asia", "Pan Janek");
    begin
-      accept Start(Consumer_Number: in Consumer_Type;
-		     Consumption_Time: in Integer) do
-	 Random_Consumption.Reset(G);	--  ustaw generator
+      accept Zacznij(Numer_Konsumenta: in Typ_Konsumenta;
+                     Czas_Konsumpcji: in Integer) do
+         Losowa_Konsumpcja.Reset(G);	--  ustaw generator
+         Losowy_Zestaw.Reset(G2);
+         Nr_Konsumenta := Numer_Konsumenta;
+         Konsumpcja := Czas_Konsumpcji;
+         CZAS := Losowa_Konsumpcja.Random(G);
+         Wydano := false;
+      end Zacznij;
+      Put_Line("KONSUMENT: Konsument to " & Nazwa_Konsumenta(Nr_Konsumenta));
+      Put_Line("KONSUMENT: Przewidywany czas konsumpcji to " & Integer'Image(CZAS) & " s.");
+      select
+         delay Duration(20.0);
+         Put_Line("BUFOR: Klient zrezygnowal z zamowienia");
+      then abort
+         loop
+            delay Duration(CZAS); --  symuluj konsumpcje
+            Rodzaj_Zestawu := Losowy_Zestaw.Random(G2);
+            Put_Line("KONSUMENT: " & Nazwa_Konsumenta(Nr_Konsumenta) & " zamowil/a " & Nazwa_Zestawu(Rodzaj_Zestawu));
+            -- pobierz zestaw do konsumpcji
+            loop
+               select
+                  B.Wydaj(Rodzaj_Zestawu, Numer_Zestawu, Wydano);
+                  if Wydano then
+                     Put_Line("KONSUMENT: " & Nazwa_Konsumenta(Nr_Konsumenta) & ": pobrano zestaw " &
+                                Nazwa_Zestawu(Rodzaj_Zestawu) & " numer " &
+                                Integer'Image(Numer_Zestawu + 1));
+                     exit;
+                  else
+                     Put_Line("BUFOR: Brakuje skladnikow do zlozenia kanapki");
+                     delay Duration(10.0);
+                  end if;
+               else
+                  Put_Line("BUFOR: Prosze poczekac w kolejce!");
+                  delay Duration(5.0);
+               end select;
+            end loop;
+         end loop;
+      end select;
+   end Konsument;
 
-	 Consumer_Nb := Consumer_Number;
-	 Consumption := Consumption_Time;
-      end Start;
-      Put_Line("Started consumer " & Consumer_Name(Consumer_Nb));
-      loop
-         delay Duration(Random_Consumption.Random(G)); --  simulate consumption
-         Assembly_Type := Random_Assembly.Random(G2);
-         -- take an assembly for consumption
-         B.Deliver(Assembly_Type, Assembly_Number);
-         Put_Line(Consumer_Name(Consumer_Nb) & ": taken assembly " &
-		    Assembly_Name(Assembly_Type) & " number " &
-		    Integer'Image(Assembly_Number));
-      end loop;
-   end Consumer;
+   task body Bufor is
+      Wyjatek: exception;
+      Pojemnosc_Magazynu: constant Integer := 30;
+      Magazyn: array (Podprodukt_Type) of Integer
+        := (0, 0, 0, 0, 0);
+      Sklad_Zestawu: array(Typ_Zestawow, Podprodukt_Type) of Integer
+        := ((0, 2, 2, 0, 1),
+            (1, 2, 0, 0, 1),
+            (0, 2, 0, 1, 0));
+      Numer_Zestawu: array(Typ_Zestawow) of Integer
+        := (1, 1, 1);
+      W_Magazynie: Integer := 0;
 
-   task body Buffer is
-      Storage_Capacity: constant Integer := 30;
-      type Storage_type is array (Product_Type) of Integer;
-      Storage: Storage_type := (0, 0, 0, 0, 0);
-      
-      Assembly_Content: array(Assembly_Type, Product_Type) of Integer
-	:= ((2, 1, 2, 1, 2),
-	    (2, 2, 0, 1, 0),
-     (1, 1, 2, 0, 1));
-      
-      Max_Assembly_Content: array(Product_Type) of Integer;
-      Assembly_Number: array(Assembly_Type) of Integer := (1, 1, 1);
-      
-      In_Storage: Integer := 0;
-
-      procedure Setup_Variables is
+      function Mozna_Przyjac(Wyrob: Podprodukt_Type) return Boolean is
       begin
-      for W in Product_Type loop
-         Max_Assembly_Content(W) := 0;
-         for Z in Assembly_Type loop
-            if Assembly_Content(Z, W) > Max_Assembly_Content(W) then
-               Max_Assembly_Content(W) := Assembly_Content(Z, W);
+         if W_Magazynie >= Pojemnosc_Magazynu then
+            raise Wyjatek;
+            return False;
+         else
+            return True;
+         end if;
+      end Mozna_Przyjac;
+
+      function Mozna_Wydac(Zestaw: Typ_Zestawow) return Boolean is
+      begin
+         for W in Podprodukt_Type loop
+            if Magazyn(W) < Sklad_Zestawu(Zestaw, W) then
+               return False;
             end if;
-	     end loop;
-	 end loop;
-      end Setup_Variables;
+         end loop;
+         return True;
+      end Mozna_Wydac;
 
-      function Can_Accept(Product: Product_Type) return Boolean is
-	 Free: Integer;		--  free room in the storage
-	 -- how many products are for production of arbitrary assembly
-	 Lacking: array(Product_Type) of Integer;
-	 -- how much room is needed in storage to produce arbitrary assembly
-	 Lacking_room: Integer;
-	 MP: Boolean;			--  can accept
+      procedure Sklad_Magazynu is
       begin
-	 if In_Storage >= Storage_Capacity then
-	    return False;
-	 end if;
-	 -- There is free room in the storage
-	 Free := Storage_Capacity - In_Storage;
-	 MP := True;
-	 for W in Product_Type loop
-	    if Storage(W) < Max_Assembly_Content(W) then
-	       MP := False;
-	    end if;
-	 end loop;
-	 if MP then
-	    return True;		--  storage has products for arbitrary
-	       				--  assembly
-	 end if;
-	 if Integer'Max(0, Max_Assembly_Content(Product) - Storage(Product)) > 0 then
-	    -- exactly this product lacks
-	    return True;
-	 end if;
-	 Lacking_room := 1;			--  insert current product
-	 for W in Product_Type loop
-	    Lacking(W) := Integer'Max(0, Max_Assembly_Content(W) - Storage(W));
-	    Lacking_room := Lacking_room + Lacking(W);
-	 end loop;
-	 if Free >= Lacking_room then
-	    -- there is enough room in storage for arbitrary assembly
-	    return True;
-	 else
-	    -- no room for this product
-	    return False;
-	 end if;
-      end Can_Accept;
-
-      function Can_Deliver(Assembly: Assembly_Type) return Boolean is
-      begin
-	 for W in Product_Type loop
-	    if Storage(W) < Assembly_Content(Assembly, W) then
-	       return False;
-	    end if;
-	 end loop;
-	 return True;
-      end Can_Deliver;
-
-      procedure Storage_Contents is
-      begin
-	 for W in Product_Type loop
-	    Put_Line("Storage contents: " & Integer'Image(Storage(W)) & " "
-		       & Product_Name(W));
-	 end loop;
-      end Storage_Contents;
+         for W in Podprodukt_Type loop
+            Put_Line("BUFOR: Sklad magazynu: " & Integer'Image(Magazyn(W)) & " "
+                     & Nazwa_Wyrobu(W));
+         end loop;
+      end Sklad_Magazynu;
 
    begin
-      Put_Line("Buffer started");
-      Setup_Variables;
+      Put_Line("BUFOR: Bufor rozpoczal prace.");
       loop
-	 accept Take(Product: in Product_Type; Number: in Integer) do
-	   if Can_Accept(Product) then
-	      Put_Line("Accepted product " & Product_Name(Product) & " number " &
-		Integer'Image(Number));
-	      Storage(Product) := Storage(Product) + 1;
-	      In_Storage := In_Storage + 1;
-  	   else
-	      Put_Line("Rejected product " & Product_Name(Product) & " number " &
-		    Integer'Image(Number));
-	   end if;
-	 end Take;
-	 Storage_Contents;
-	 accept Deliver(Assembly: in Assembly_Type; Number: out Integer) do
-	    if Can_Deliver(Assembly) then
-	       Put_Line("Delivered assembly " & Assembly_Name(Assembly) & " number " &
-			  Integer'Image(Assembly_Number(Assembly)));
-	       for W in Product_Type loop
-		  Storage(W) := Storage(W) - Assembly_Content(Assembly, W);
-		  In_Storage := In_Storage - Assembly_Content(Assembly, W);
-	       end loop;
-	       Number := Assembly_Number(Assembly);
-	       Assembly_Number(Assembly) := Assembly_Number(Assembly) + 1;
-	    else
-	       Put_Line("Lacking products for assembly " & Assembly_Name(Assembly));
-	       Number := 0;
-	    end if;
-	 end Deliver;
-	 Storage_Contents;
+         select
+            accept Przyjmij(Wyrob: in Podprodukt_Type; Numer: in Integer) do
+               begin
+                  if Mozna_Przyjac(Wyrob) then
+                     Put_Line("BUFOR: Przyjeto komponent: " & Nazwa_Wyrobu(Wyrob) & " nr " &
+                                Integer'Image(Numer));
+                     Magazyn(Wyrob) := Magazyn(Wyrob) + 1;
+                     W_Magazynie := W_Magazynie + 1;
+                  else
+                     Put_Line("BUFOR: Odrzucono komponent:  " & Nazwa_Wyrobu(Wyrob) & " nr " &
+                                Integer'Image(Numer));
+                  end if;
+                  Sklad_Magazynu;
+               exception
+                  when Wyjatek =>
+                     Put_Line("MAGAZYN: Magazyn jest przepelniony! Komponent nie zostanie przyjety!");
+               end;
+            end Przyjmij;
+         or
+            accept Wydaj(Zestaw: in Typ_Zestawow; Numer: out Integer; Wydano: out Boolean) do
+               if Mozna_Wydac(Zestaw) then
+                  Put_Line("BUFOR: Wydano zestaw " & Nazwa_Zestawu(Zestaw) & " nr " &
+                             Integer'Image(Numer_Zestawu(Zestaw) + 1));
+                  for W in Podprodukt_Type loop
+                     Magazyn(W) := Magazyn(W) - Sklad_Zestawu(Zestaw, W);
+                     W_Magazynie := W_Magazynie - Sklad_Zestawu(Zestaw, W);
+                  end loop;
+                  Numer := Numer_Zestawu(Zestaw);
+                  Numer_Zestawu(Zestaw) := Numer_Zestawu(Zestaw) + 1;
+                  Wydano := true;
+               else
+                  Put_Line("BUFOR: Brak czesci dla zestawu " & Nazwa_Zestawu(Zestaw));
+                  Numer := 0;
+                  Wydano := false;
+               end if;
+               Sklad_Magazynu;
+            end Wydaj;
+         end select;
       end loop;
-   end Buffer;
-   
+   end Bufor;
+
 begin
    for I in 1 .. Number_Of_Products loop
-      P(I).Start(I, 10);
+      P(I).Zacznij(I, 10);
    end loop;
    for J in 1 .. Number_Of_Consumers loop
-      K(J).Start(J,12);
+      K(J).Zacznij(J,12);
    end loop;
 end Simulation;
+
 
